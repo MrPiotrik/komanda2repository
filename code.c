cb61c16833aa3724b5874b1eb488b97e832a008d
MrPiotrik <95098731+MrPiotrik@users.noreply.github.com>
2021-11-30 22:11:09 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 14 //розмір масиву
+#include <time.h>
 int main() {
-    int i, A[14]={23, 46, -11, -43, 99, -34, -67, -22, -89, 4, 12, 55, 68, -31}; 
+    int i, dimArray, A[dimArray]; 
     //i-індекс елементу масиву
-    //A[14]-заданий масив
+    //A[i]-заданий масив
+    printf("dimArray=");
+    scanf("%i", &dimArray);
+    srand(time(NULL));
+    for (i = 0; i < dimArray; i++) {
+    A[i] = 50-rand() % 100;
+    }
     printf("Array:\n");
-    for (i = 0; i<N; i++) {
+    for(i=0;i<dimArray;i++){
         printf("%i ", A[i]);
     }
     printf("\n");
@@ -14,14 +20,14 @@ int main() {
     //sum-сума від'ємних елементів
     //k-кількість від'ємних елементів
     //saNeg-середнє арифметичне від'ємних елементів
-    for(i=0;i<N;i++){
+    for(i=0;i<dimArray;i++){
         if(A[i]<0){
             sum+=A[i];
             k++;
         }
     }
     saNeg=sum/k;
-    for(i=0;i<N;i++){
+    for(i=0;i<dimArray;i++){
         ind=0;
         if((i%2==0)&&(A[i]<0)){
             ind=i;
@@ -32,32 +38,32 @@ int main() {
     //suma-сума додатніх елементів
     //n-кі-сть додатніх елементів
     //saPos-середнє арифметичне додатніх елементів
-    for(i=0;i<N;i++){
+    for(i=0;i<dimArray;i++){
         if(A[i]>0){
             suma+=A[i];
             n++;
         }
     }
     saPos=suma/n;
-    for(i=0;i<N;i++){
+    for(i=0;i<dimArray;i++){
         index=0;
         if(i%2==1){
             index=i;
             A[index]=saPos;
         }
     }
-    if(k==N){
-        for(i=0;i<N;i++){
+    if(k==dimArray){
+        for(i=0;i<dimArray;i++){
             A[i]=0;
         }
     }
-    if(n==N){
-        for(i=0;i<N;i++){
+    if(n==dimArray){
+        for(i=0;i<dimArray;i++){
             A[i]=1;
         }
     }
     printf("Result:\n");
-    for (i = 0; i < N; i++) {
+    for(i=0;i<dimArray;i++){
         printf("%i ", A[i]);
         }
 return 0;
