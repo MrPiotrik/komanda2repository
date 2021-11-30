ce576cf9f16a09ecdf932276be5f79a0621d5cd9
MrPiotrik <95098731+MrPiotrik@users.noreply.github.com>
2021-11-30 21:30:33 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,4 +1,16 @@
-int sum=0, k=0, ind, saNeg;
+#include <stdio.h>
+#include <stdlib.h>
+#define N 14 //розмір масиву
+int main() {
+    int i, A[14]={23, 46, -11, -43, 99, -34, -67, -22, -89, 4, 12, 55, 68, -31}; 
+    //i-індекс елементу масиву
+    //A[14]-заданий масив
+    printf("Array:\n");
+    for (i = 0; i<N; i++) {
+        printf("%i ", A[i]);
+    }
+    printf("\n");
+    int sum=0, k=0, ind, saNeg;
     //sum-сума від'ємних елементів
     //k-кількість від'ємних елементів
     //saNeg-середнє арифметичне від'ємних елементів
@@ -34,3 +46,19 @@ int sum=0, k=0, ind, saNeg;
             A[index]=saPos;
         }
     }
+    if(k==N){
+        for(i=0;i<N;i++){
+            A[i]=0;
+        }
+    }
+    if(n==N){
+        for(i=0;i<N;i++){
+            A[i]=1;
+        }
+    }
+    printf("Result:\n");
+    for (i = 0; i < N; i++) {
+        printf("%i ", A[i]);
+        }
+return 0;
+}
